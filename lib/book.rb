8cbdbb0a2ecbd07427b26eb494f4ee1fbb0d9ffe
Amy Nguyen <amy.nguyen730@gmail.com>
2019-10-09 13:21:31 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,13 +2,8 @@ class Book
   attr_accessor :author
   attr_accessor :page_count
   attr_accessor :genre
+  attr_reader :title
   def initialize(title)
     @title = title
   end
-  
-  def title
-    @title
-  end
-  
-  
 end
